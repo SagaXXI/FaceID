1e5ae4e03b91af84855abd2455ecf455a1817ce6
SagaXXI <77396998+SagaXXI@users.noreply.github.com>
2023-04-11 22:57:19 +0600

    Update EncodeGenerator.py

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -9,8 +9,8 @@ from firebase_admin import  storage
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://faceid-20413-default-rtdb.firebaseio.com/",
-    'storageBucket': "faceid-20413.appspot.com"
+    'databaseURL': "",
+    'storageBucket': ""
 })
 
 
